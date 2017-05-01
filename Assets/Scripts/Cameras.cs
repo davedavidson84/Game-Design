e5bf57e88fe5a3acc8d4e9c215b02a1fc1721f2e
davedavidson84 <buckhunterclagg@gmail.com>
2017-05-01 15:47:10 -0600

    Camera Lerp

--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -6,12 +6,19 @@ public class Cameras : MonoBehaviour
 	public Transform player = null;
 	public Transform target = null;
 
+	public Vector3 speed = new Vector3 (4.0f, 2.0f, 1.0f);
+
+	public Vector3 nextPosition = Vector3.zero;
 
 	void LateUpdate ()
 	{
-		//this.transform.LookAt ( player.position );
+		nextPosition.x = Mathf.Lerp (this.transform.position.x, target.position.x, speed.x * Time.deltaTime);
+		nextPosition.y = Mathf.Lerp (this.transform.position.y, target.position.y, speed.y * Time.deltaTime);
+		nextPosition.z = Mathf.Lerp (this.transform.position.z, target.position.z, speed.z * Time.deltaTime);
 
-		this.transform.position = target.position;
+		this.transform.position = nextPosition;
+
+		this.transform.LookAt ( player.position );
 	}
 
 
