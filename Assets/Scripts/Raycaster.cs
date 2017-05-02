25b3294e8f2fae14755a05b8598f9c62fb0645da
davedavidson84 <buckhunterclagg@gmail.com>
2017-05-02 15:48:27 -0600

    Raycast
    Raycast Objects in the Scene

---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour 
+{
+	public Camera camera;
+
+	private Ray ray;
+	private RaycastHit hit;
+
+	void Update ()
+	{
+		if ( Input.GetMouseButtonDown ( 0 ) ) 
+		{
+			ray = camera.ScreenPointToRay (Input.mousePosition);
+
+			if (Physics.Raycast (ray)) 
+			{
+				Debug.Log ("Hit something.");
+			}
+		}
+	}
+}
