230260c98bfb250c33b5d0f29317d0c2e2b43536
davedavidson84 <buckhunterclagg@gmail.com>
2017-05-02 15:47:12 -0600

    Camera
    Camera States, Camera Triggers, and Camera Minimap

---
+++ Assets/Scripts/CameraTriggers.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CameraTriggers : MonoBehaviour
+{
+	public Cameras cameras;
+	public Cameras.CameraState cameraState;
+
+	void OnTriggerEnter ( Collider other )
+	{
+		if (other.name == "player") 
+		{
+			cameras.cameraState = cameraState;
+		}
+	}
+}
--- Assets/Scripts/Cameras.cs
+++ Assets/Scripts/Cameras.cs
@@ -10,16 +10,30 @@ public class Cameras : MonoBehaviour
 
 	public Vector3 nextPosition = Vector3.zero;
 
+	public enum CameraState { none, followPosition, lookAtPlayer, both };
+	public CameraState cameraState = CameraState.none;
+
 	void LateUpdate ()
+	{
+		switch (cameraState) {
+		case CameraState.none: 												 break;
+		case CameraState.followPosition: FollowPosition (); 				 break;
+		case CameraState.lookAtPlayer: 	 LookAtPlayer   (); 				 break;
+		case CameraState.both: 			 FollowPosition (); LookAtPlayer (); break;
+		}
+	}
+
+	void FollowPosition ()
 	{
 		nextPosition.x = Mathf.Lerp (this.transform.position.x, target.position.x, speed.x * Time.deltaTime);
 		nextPosition.y = Mathf.Lerp (this.transform.position.y, target.position.y, speed.y * Time.deltaTime);
 		nextPosition.z = Mathf.Lerp (this.transform.position.z, target.position.z, speed.z * Time.deltaTime);
 
 		this.transform.position = nextPosition;
-
-		this.transform.LookAt ( player.position );
 	}
 
-
+	void LookAtPlayer ()
+	{
+		this.transform.LookAt ( player.position );
+	}
 }
